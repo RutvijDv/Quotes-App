5b33fde57eb099c5ff58b5be8d080b159544c0e4
Rutvij Vamja <rutvij.vamja@gmail.com>
2021-12-17 22:11:29 +0530

    add http request

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,15 +1,42 @@
+import { useEffect } from "react";
 import QuotesList from "./../components/quotes/QuoteList";
-
-const DUMMY_QUOTES = [
-  { id: "q1", author: "p1", text: "learnreact1" },
-  { id: "q2", author: "p2", text: "learnreact2" },
-];
+import useHttp from "./../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
+import LoadingSpinner from "./../components/UI/LoadingSpinner";
+import NoQuotesFound from "./../components/quotes/NoQuotesFound";
 
 const AllQuote = () => {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
   return (
     <>
       <h1>All Quote</h1>
-      <QuotesList quotes={DUMMY_QUOTES} />
+      <QuotesList quotes={loadedQuotes} />
     </>
   );
 };
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import QuoteForm from "./../components/quotes/QuoteForm";
+import useHttp from "./../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
+
   const history = useHistory();
 
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+    sendRequest(quoteData);
 
     history.push("/quotes");
   };
@@ -13,7 +24,10 @@ const NewQuote = () => {
   return (
     <>
       <h1>New Quote</h1>
-      <QuoteForm onAddQuote={addQuoteHandler} />
+      <QuoteForm
+        isLoading={status === "pending"}
+        onAddQuote={addQuoteHandler}
+      />
     </>
   );
 };
